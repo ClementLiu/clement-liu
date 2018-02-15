b9552b0dc878c51d407d1cfacad704430c492e9a
ClementLiu <ziwei.liu@cca.edu>
2018-02-14 17:27:37 -0800

    add changing background color

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ jQuery(document).ready(function($) {
         $(this).attr('id', 'decor' + index);
         locationsp.push($(this).offset().top + 210);
     });
+    // select background color
+    $(".decorBack1").each(function(index) {
+        // console.log($(this).offset());
+        $(this).attr('id', 'decorB' + index);
+        // locationsp.push($(this).offset().top + 210);
+    });
+
     $(".sideText").each(function(index, el) {
         $(this).css({ 'transform': 'rotate(-90deg) translate( -' + locationsp[index] + 'px, 81vw)' });
 
@@ -17,13 +24,31 @@ jQuery(document).ready(function($) {
             opacity: 0.1,
             ease: Power2.easeInOut
         });
+
+        // add backgroundColor changing
+
+        var colorChange = TweenMax.staggerFromTo("#decorB" + index, .8, {
+            filter: "contrast(0%)",
+        }, {
+            filter: "contrast(100%)",
+            ease: Power2.easeInOut
+        });
+
         var scene = new ScrollMagic.Scene({
                 triggerElement: "#decor" + index,
                 offset: -300
             })
             .setTween(opacityChange);
-            // .addTo(controller); // assign the scene to the controller
+
+        // add change background scene
+        var sceneColor = new ScrollMagic.Scene({
+                triggerElement: "#decor" + index,
+                offset: -300
+            })
+            .setTween(colorChange);
+        // .addTo(controller); // assign the scene to the controller
         sideAnim.push(scene);
+        sideAnim.push(sceneColor);
     });
     controller.addScene(sideAnim);
 
