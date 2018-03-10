5ec7ff94fb899f11fcad08348e2e08a33311b339
ClementLiu <ziwei.liu@cca.edu>
2018-03-09 17:31:01 -0800

    Add SCSS set up

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./style/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
