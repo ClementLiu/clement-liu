aa8d223f0ab4f0f1b6baf99b3aa80ba4ed6a0638
ClementLiu <ziwei.liu@cca.edu>
2018-03-12 00:50:35 -0700

    Responsive

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['styles'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -9,10 +23,12 @@ gulp.task('styles', function() {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./style/'));
+        .pipe(gulp.dest('./style/'))
+        .pipe(browserSync.stream());
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
-});
+// gulp.task('default',function() {
+//     gulp.watch('sass/**/*.scss',['styles']);
+// });
+gulp.task('default', ['serve']);
